886aa72af85b5c0f90d77b35de7cd42bfbfb0251
Jacob Daughtry <akutozo@gmail.com>
2020-08-23 12:06:33 -0500

    Added Main Menu Inquirer Prompts

--- server.js
+++ server.js
@@ -1,19 +1,66 @@
 // get the client
 const mysql = require('mysql2');
+
+// NPM installations
+const inquirer = require('inquirer');
+
+// Additional Requirements
+const fs = require("fs")
+
  
 // create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
+  port: '3005',
   user: 'root',
-  database: 'test'
+  password: 'swordfish',
+  database: 'emp_mgr_db'
 });
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  start();
+});
+
+//Application Runs
+runEmpMgr = () => {
+    console.log(`
+        Welcome to the Employee Management Interface.
+    `
+    );
+    empMgrMenu();
+};
+
+empMgrMenu = () => {
+    console.log(`Accessing Database...`)
+    inquirer.prompt(mainmenu);
+};
+
+const mainmenu = [
+  {
+    type: 'list',
+    name: 'mainmenu',
+    message: 'What do you need to do today?',
+    choices: 
+      [' View All Departments ', 
+       ' View All Roles ', 
+       ' View All Employees ', 
+       ' Add a Department ', 
+       ' Add a Role ', 
+       ' Add An Employee ', 
+       ' Update Employee Role '
+      ]
+  }
+]
+
  
 // simple query
 connection.query(
   'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
   function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+    // console.log(results); // results contains rows returned by server
+    // console.log(fields); // fields contains extra meta data about results, if available
   }
 );
  
@@ -22,6 +69,9 @@ connection.query(
   'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
   ['Page', 45],
   function(err, results) {
-    console.log(results);
+    // console.log(results);
   }
 );
+
+
+runEmpMgr();
